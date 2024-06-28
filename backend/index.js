b680682152ed6cac912a0fca60391d01287087d8
PrranitArora <116321877+PrranitArora@users.noreply.github.com>
2024-06-28 00:06:58 -0400

    server reruns per save

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('<h1>Hello from server!!</h1>')
+    res.send('<h1>Hello from server!!!</h1>')
 })
 
 app.listen(5000, () => {
