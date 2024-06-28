c59b3e93ff829bd1613a23d28fc64d4de48affe6
PrranitArora <116321877+PrranitArora@users.noreply.github.com>
2024-06-28 00:03:10 -0400

    backend changes

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('<h1>Hello from server</h1>')
+    res.send('<h1>Hello from server!!</h1>')
 })
 
 app.listen(5000, () => {
     console.log('Port is listening.');
 });
+
 
 
