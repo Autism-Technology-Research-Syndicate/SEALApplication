bbfaba204667490c4bc14d82e2ec030421bee8a2
PrranitArora <116321877+PrranitArora@users.noreply.github.com>
2024-07-08 14:33:46 -0400

    updated app.tsx

--- App.tsx
+++ App.tsx
@@ -77,8 +77,8 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change the
+            screen.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
@@ -87,7 +87,7 @@ function App(): React.JSX.Element {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            Visit Autrs.com, or email Inquiries@autrs.com:
           </Section>
           <LearnMoreLinks />
         </View>
