b3cad86e302172b3e6e98f315d6f60f5278765a6
angelalala <angela9004@gmail.com>
2024-07-21 20:47:08 -0700

    feat: export db function

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ import {
   printFirstRow
 } from './Database/dbInitialization';
 
+import { exportDatabase, shareDatabase } from './Database/dbExport';
+
 AppRegistry.registerComponent(appName, () => App);
 
 const setupDatabase = async () => {
@@ -38,3 +40,15 @@ const setupDatabase = async () => {
 };
 
 setupDatabase();
+// if want to export the database
+//  .then(() => {
+//    console.log('### Database setup completed');
+//    return exportDatabase();
+//  })
+//  .then((exportPath) => {
+//    console.log('### Database export completed');
+//    return shareDatabase(exportPath);
+//  })
+//  .catch((error) => {
+//    console.error('### Error:', error);
+//  });
