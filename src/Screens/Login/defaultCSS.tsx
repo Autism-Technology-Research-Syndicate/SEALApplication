4a019ea7a1a7ce127151c752f5bfa9c34e258312
objectbb <objectbb@gmail.com>
2024-08-02 14:14:18 -0500

    fixed login layout

--- src/Screens/Login/defaultCSS.tsx
+++ src/Screens/Login/defaultCSS.tsx
@@ -2,35 +2,30 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
+        flexDirection:'column',
         height: '100%',
         width: '100%',
         rowGap: 10,
-        padding: 10,
-
+        padding: 10
     },
-    subheader: {
-        fontFamily: 'Helvetica Neue',
-        fontSize: 18,
-        fontStyle: 'normal',
-        fontWeight: '400',
-        alignSelf: 'center',
-        color: '#305070'
-
+    body: {
+        flex: 2,
+        rowGap: 50,
+        justifyContent: 'center'
     },
-    header: {
-        fontFamily: 'Comic Neue',
-        fontSize: 27,
-        fontStyle: 'normal',
-        fontWeight: 'bold',
-        alignSelf: 'center',
-        color: '#305070'
-
-    },
-    section:
+    upper_body:
     {
-        flex: 1,
-        justifyContent: 'flex-end',
+        justifyContent: 'center',
+        rowGap: 20
+
+    },
+    bottom_body:
+    {
+        rowGap: 20
+    },
+    middle_body:
+    {
         rowGap: 10
-    }
+    },
 });
 export default styles;
