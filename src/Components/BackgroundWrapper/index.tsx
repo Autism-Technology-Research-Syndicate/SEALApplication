5975e364d4bf7fc2682fcb645099cb3d77a142ef
objectbb <objectbb@gmail.com>
2024-08-02 17:02:29 -0500

    merge bb-pre-alpha

--- src/Components/BackgroundWrapper/index.tsx
+++ src/Components/BackgroundWrapper/index.tsx
@@ -1,19 +1,10 @@
-import { ImageBackground, Text, View } from 'react-native';
+import { View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import styles from './defaultCSS';
 
-const image = require('../../Assets/images/backgroundImage.png');
-
 const Index = ({ children }) => {
     return (
-        <View style={styles.container}>
-           {/* <ImageBackground source={image} resizeMode="stretch" style={styles.image}>
-                <View>
-                {children}
-                </View >
-            </ImageBackground> */}
-
-            
+        <View style={styles.container}>           
             <LinearGradient style={styles.background} useAngle={true} angle={45} angleCenter={{x:0.5,y:0.35}} end={{x: 1, y: 0}} colors={['#D2F9F9', '#f5f5f5','#F9E2D2']}>
             <View>
                 {children}
