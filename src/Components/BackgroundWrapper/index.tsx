f918bb55c2e321167919355562e022beaae45656
objectbb <objectbb@gmail.com>
2024-08-02 13:09:29 -0500

    remove camera Home screen

--- src/Components/BackgroundWrapper/index.tsx
+++ src/Components/BackgroundWrapper/index.tsx
@@ -12,6 +12,8 @@ const Index = ({ children }) => {
                 {children}
                 </View >
             </ImageBackground> */}
+
+            
             <LinearGradient style={styles.background} useAngle={true} angle={45} angleCenter={{x:0.5,y:0.35}} end={{x: 1, y: 0}} colors={['#D2F9F9', '#f5f5f5','#F9E2D2']}>
             <View>
                 {children}
