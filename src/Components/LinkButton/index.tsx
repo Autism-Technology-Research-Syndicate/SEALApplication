7b179f72cbdb38c9de88bdde6cb78f2b26a7fd85
objectbb <objectbb@gmail.com>
2024-08-04 16:41:06 -0500

    text font size larger

--- src/Components/LinkButton/index.tsx
+++ src/Components/LinkButton/index.tsx
@@ -6,8 +6,8 @@ import { Button } from 'react-native-paper';
 function Index(props) {
   const { onPress, title = 'Heeelo' } = props;
 
-  return (  <Button mode="text" {...props}  onPress={onPress} >
-    <Text style={[styles.text, props.style]}>{title}</Text>
+  return (  <Button labelStyle={[styles.text, props.style]} mode="text" {...props}  onPress={onPress} >
+   {title}
   </Button>);
 
 };
--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -14,8 +14,8 @@ const Index = ({ navigation }) => {
         <View style={styles.body}>
           <View style={styles.upper_body}>
             <TextField placeholder="Please enter username" label="username" />
-            <TextField placeholder="Please enter password" label="password"  />
-            <LinkButton title='Forget password?' style={{flexDirection: 'row-reverse' }} onPress={() => navigation.navigate('Welcome')} />
+            <TextField placeholder="Please enter password" label="password" validationType="password"  />
+            <LinkButton title='Forget password?' style={{flexDirection: 'row-reverse', fontSize: 20 }} onPress={() => navigation.navigate('Welcome')} />
           </View>
           <View style={styles.bottom_body}>
           <Button title='Submit' onPress={() => navigation.navigate('Personal')} />
