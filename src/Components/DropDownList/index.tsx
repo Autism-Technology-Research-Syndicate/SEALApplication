fd150a3689c260b2ebecc36f30b6fe48be5505f0
Rexhao362 <56262178+Rexhao362@users.noreply.github.com>
2024-08-05 17:51:27 +0100

    combined dropdown functions, added docstrings

--- src/Components/DropDownList/index.tsx
+++ src/Components/DropDownList/index.tsx
@@ -2,35 +2,47 @@ import React, {useState} from 'react';
 import DropDownPicker from 'react-native-dropdown-picker';
 import styles from './defaultCSS';
 
-export const transformLabels = (labels: string[]) => {
-  return labels.map(e => ({label: e, value: e.toLowerCase()}));
-};
-
+/**
+ * Transforms an array of strings into an array of objects with label and value properties.
+ * Creates a dropdown picker component with the transformed labels.
+ *
+ * @param {string[]} labels - Array of label strings.
+ * @param {string[]} value - Array of selected values.
+ * @param {Function} setValue - Function to update the selected values.
+ * @returns {React.JSX.Element} - The rendered dropdown picker component.
+ */
 export const createDropDown = (
-  labels: object[],
+  labels: string[],
   value: string[],
   setValue: any,
 ) => {
+  // Transform the label strings into objects with label and value properties
+  const transformedLabels = labels.map(e => ({label: e, value: e.toLowerCase()}));
+
+  // State to manage the dropdown open/close status
   const [open, setOpen] = useState(false);
-  const [items, setItems] = useState(labels);
+
+  // State to manage the dropdown items list
+  const [items, setItems] = useState(transformedLabels);
 
   return (
     <DropDownPicker
-      placeholder="Select All That Apply"
-      open={open}
-      value={value}
-      items={items}
+      placeholder="Select All That Apply" 
+      open={open} 
+      value={value} 
+      items={items} 
       setOpen={setOpen}
       setValue={setValue}
       setItems={setItems}
       multiple={true}
       mode='BADGE'
       listMode='MODAL'
-      min={0}
-      max={5}
-      showTickIcon={true}
-      dropDownDirection="AUTO"
-      style={styles.dropDown}
+      min={0} // Minimum number of selections
+      max={5} // Maximum number of selections
+      showTickIcon={true} 
+      dropDownDirection="AUTO" 
+      style={styles.dropDown} 
+      labelStyle={styles.labels}
     />
   );
 };
