7120116d4d74aa73abe212c8d9e0e8cadd3120cf
Rexhao362 <56262178+Rexhao362@users.noreply.github.com>
2024-08-05 17:51:59 +0100

    Added border colour and font size

--- src/Components/DropDownList/defaultCSS.tsx
+++ src/Components/DropDownList/defaultCSS.tsx
@@ -3,7 +3,11 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   dropDown: {
     marginBottom: 15,
+    borderColor: '#305070',
   },
+  labels: {
+    fontSize: 15,
+  }
 });
 
 export default styles;
