0c8338954a1e4a239ebddfb6da05ffa8c0bb8a10
objectbb <objectbb@gmail.com>
2024-08-14 16:34:47 -0500

    failed implementation using fast-tflite

--- src/Library/Camera/VisionCamera.js
+++ src/Library/Camera/VisionCamera.js
@@ -1,14 +1,22 @@
 import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from 'react-native-vision-camera';
+import { loadTensorflowModel } from 'react-native-fast-tflite';
 import { StyleSheet, useState } from 'react-native';
 
 
+
 export function VisionCamera() {
-  const device = useCameraDevice('back');
+  const model = loadTensorflowModel(require('./model.tflite'))
+
+  const device = useCameraDevice('back', {
+    physicalDevices: ['wide-angle-camera']
+  });
   const { hasPermission, requestPermission } = useCameraPermission();
 
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet'
-    console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)
+    //console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)
+    const outputData =  model.run(frame)
+    console.log(outputData)
   }, [])
 
   return (
@@ -19,6 +27,7 @@ export function VisionCamera() {
       isActive={true}
       frameProcessor={frameProcessor}
       frameProcessorFps={5}
+      photoQualityBalance="speed"
     />
   );
 }
