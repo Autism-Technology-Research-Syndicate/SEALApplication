36363a0cc3bc98714b1166499f21d50498493f3d
objectbb <objectbb@gmail.com>
2024-08-20 21:36:09 -0500

    Vision Camera capturing frame

--- src/Library/Camera/VisionCamera.js
+++ src/Library/Camera/VisionCamera.js
@@ -1,11 +1,9 @@
 import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from 'react-native-vision-camera';
-import { loadTensorflowModel } from 'react-native-fast-tflite';
 import { StyleSheet, useState } from 'react-native';
 
 
 
 export function VisionCamera() {
-  const model = loadTensorflowModel(require('./model.tflite'))
 
   const device = useCameraDevice('back', {
     physicalDevices: ['wide-angle-camera']
@@ -14,9 +12,9 @@ export function VisionCamera() {
 
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet'
-    //console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)
-    const outputData =  model.run(frame)
-    console.log(outputData)
+    console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)
+    //const outputData =  model.run(frame)
+   // console.log(outputData)
   }, [])
 
   return (
