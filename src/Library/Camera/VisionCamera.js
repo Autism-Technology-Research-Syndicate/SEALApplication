b1d0a03af27d978e04e547801c7eadd735b79fba
objectbb <objectbb@gmail.com>
2024-08-25 00:02:19 -0400

    figuring out image processing...what is the expected image input?

--- src/Library/Camera/VisionCamera.js
+++ src/Library/Camera/VisionCamera.js
@@ -1,28 +1,26 @@
 import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from 'react-native-vision-camera';
-//import { loadTensorflowModel } from 'react-native-fast-tflite';
-import { StyleSheet, useState } from 'react-native';
-import { useImageLabeler } from 'react-native-vision-camera-image-labeler';
+import { useTensorflowModel } from 'react-native-fast-tflite';
+import { StyleSheet } from 'react-native';
 
 function VisionCamera() {
-  //const model = loadTensorflowModel(require('./model.tflite'))
-
-  const {scanImage} = useImageLabeler();
+  const objectDetection = useTensorflowModel(require('../model.tflite'));
+  const model = objectDetection.state === "loaded" ? objectDetection.model : undefined;
 
   const device = useCameraDevice('back', {
     physicalDevices: ['wide-angle-camera']
   });
   const { hasPermission, requestPermission } = useCameraPermission();
 
+  let outputData = '';
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet'
-   // console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)
-   let data = '';
-    if (scanImage)
-      data = scanImage(frame)
+   if (model == null) return;
+   //console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`);
 
-    console.log(data, 'data')
-    //const outputData =  model.run(frame)
-    //console.log(outputData)
+    const buffer = frame.toArrayBuffer()
+   // const data = new Uint8Array(buffer)
+    outputData =  model.run(buffer);
+    console.log(outputData);
   }, [])
 
   return (
@@ -32,7 +30,7 @@ function VisionCamera() {
       device={device}
       isActive={true}
       frameProcessor={frameProcessor}
-      frameProcessorFps={50}
+      frameProcessorFps={30}
       photoQualityBalance="speed"
     />
   );
