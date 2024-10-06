5215a8a5b2004862d5c58666cde035f11ce7bbdd
germino45 <114361293+germino45@users.noreply.github.com>
2024-10-06 15:53:40 -0500

    feature: add colorblind filters

--- App.tsx
+++ App.tsx
@@ -6,20 +6,29 @@
  * and context providers.
  */
 
-
-import React, { useCallback, useEffect, useRef } from 'react';
-import { View, TouchableWithoutFeedback, StyleSheet, TouchableOpacity, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useCallback, useEffect, useRef} from 'react';
+import {
+  View,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import Home from './src/Screens/Home/.';
 import DeveloperMode from './src/Screens/DeveloperMode';
-import { DeveloperModeProvider, useDeveloperMode } from './src/Contexts/DeveloperModeContext';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  DeveloperModeProvider,
+  useDeveloperMode,
+} from './src/Contexts/DeveloperModeContext';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {ColorblindProvider} from './src/Contexts/ColorblindContext.tsx';
+import ColorblindFilter from './src/Components/ColorblindFilter/index.tsx';
 import styles from './appCSS.tsx';
 
 // Create a stack navigator for the root of the app
 
 const RootStack = createNativeStackNavigator();
-
 
 /**
  * CloseButton Component
@@ -29,7 +38,7 @@ const RootStack = createNativeStackNavigator();
  *
  * @param {Function} onPress - Function to call when the button is pressed
  */
-const CloseButton: React.FC<{ onPress: () => void }> = ({ onPress }) => (
+const CloseButton: React.FC<{onPress: () => void}> = ({onPress}) => (
   <View style={styles.closeButtonContainer}>
     <TouchableOpacity onPress={onPress} style={styles.closeButton}>
       <Text style={styles.closeButtonText}>Close</Text>
@@ -37,38 +46,31 @@ const CloseButton: React.FC<{ onPress: () => void }> = ({ onPress }) => (
   </View>
 );
 
-
-
-
 const AppContent: React.FC = () => {
-
   // Access developer mode functions and state from context
 
-  const { isDeveloperModeActive, openDeveloperMode, closeDeveloperMode } = useDeveloperMode();
-
+  const {isDeveloperModeActive, openDeveloperMode, closeDeveloperMode} =
+    useDeveloperMode();
 
   return (
-
     <View style={styles.container}>
-
-     {/* Main navigation stack */}
-      <RootStack.Navigator screenOptions={{ headerShown: false }}>
-          <RootStack.Screen name="Stack" component={Home} />
-        </RootStack.Navigator>
+      {/* Main navigation stack */}
+      <RootStack.Navigator screenOptions={{headerShown: false}}>
+        <RootStack.Screen name="Stack" component={Home} />
+      </RootStack.Navigator>
 
       {/* Developer mode activation area (top-right corner) */}
-        <TouchableWithoutFeedback onPress={openDeveloperMode}>
+      <TouchableWithoutFeedback onPress={openDeveloperMode}>
         <View style={styles.activationArea} />
       </TouchableWithoutFeedback>
 
       {/* Render DeveloperMode component when active */}
-        {isDeveloperModeActive && <DeveloperMode/>}
+      {isDeveloperModeActive && <DeveloperMode />}
       {/* Render close button for developer mode when active */}
-        {isDeveloperModeActive && <CloseButton onPress={closeDeveloperMode} />}
-      </View>
+      {isDeveloperModeActive && <CloseButton onPress={closeDeveloperMode} />}
+    </View>
   );
 };
-
 
 /**
  * App Component
@@ -78,7 +80,6 @@ const AppContent: React.FC = () => {
  * - DeveloperModeProvider for managing developer mode state
  * - NavigationContainer for React Navigation
  */
-
 
 const App: React.FC = () => {
   // Temporatiliy commented out the camera interval code
@@ -97,14 +98,13 @@ const App: React.FC = () => {
 
   return (
     <DeveloperModeProvider>
-    <NavigationContainer>
-      <AppContent />
-    </NavigationContainer>
-  </DeveloperModeProvider>
-      );
+      <ColorblindProvider>
+        <NavigationContainer>
+          <AppContent />
+        </NavigationContainer>
+      </ColorblindProvider>
+    </DeveloperModeProvider>
+  );
 };
-
-
-
 
 export default App;
--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,7 +1,9 @@
 // src/Screens/Home/index.tsx
 
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {ColorblindProvider} from '../../Contexts/ColorblindContext';
+import ColorblindFilter from '../../Components/ColorblindFilter';
 import WelcomeToSeal from '../WelcomeToSeal';
 import Login from '../Login';
 import PersonalPage from '../PersonalPage';
@@ -10,27 +12,36 @@ import HowSealWorks from '../HowSEALWorks';
 import WebCamFeed from '../../Components/WebCamFeed';
 import Break from '../Break';
 import BottomNav from '../../Features/BottomNavigation';
+import ColorblindSettings from '../ColorblindSettings';
 
 const Stack = createNativeStackNavigator();
 
 const Index = () => {
   return (
-
-      <Stack.Navigator initialRouteName="WebCamFeed" screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen name="Welcome" component={WelcomeToSeal}  />
-        <Stack.Screen name="Login" component={Login}  />
-        <Stack.Screen name="Personal" component={PersonalPage}  />
-        <Stack.Screen name="AccountSignUp" component={AccountSignUp}  />
+    <>
+      <Stack.Navigator
+        initialRouteName="WebCamFeed"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Welcome" component={WelcomeToSeal} />
+        <Stack.Screen
+          name="ColorblindSettings"
+          component={ColorblindSettings}
+        />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Personal" component={PersonalPage} />
+        <Stack.Screen name="AccountSignUp" component={AccountSignUp} />
         {/* the "Main "screen below renders the bottom Nav which wraps the relevant pages it is included on */}
-        <Stack.Screen name="Main" component={BottomNav}  />
-        <Stack.Screen name="HowSealWorks" component={HowSealWorks}  />
-        <Stack.Screen name="Break" component={Break} />
+        <Stack.Screen name="Main" component={BottomNav} />
+        <Stack.Screen name="HowSealWorks" component={HowSealWorks} />
+        {/* Commented for testing, uncomment to enable break screen*/}
+        {/*<Stack.Screen name="Break" component={Break} /> */}
         {/* Commented for testing, to enable camera feed uncomment this */}
         {/* <Stack.Screen name="WebCamFeed" component={WebCamFeed}  /> */}
-
       </Stack.Navigator>
+      <ColorblindFilter />
+    </>
   );
 };
 
@@ -53,7 +64,6 @@ export default Index;
 
 // const Index = ({navigate}) => {
 //   return (
-
 
 //       <Stack.Navigator initialRouteName={WelcomeToSeal} screenOptions={{
 //         headerShown: false
